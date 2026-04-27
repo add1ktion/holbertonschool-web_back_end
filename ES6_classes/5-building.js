a3c257d8dc32f2885e89ce9466d2739f2f61620a
add1ktion <antoine.bats@gmail.com>
2026-04-27 13:36:59 +0200

    Fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,7 +2,6 @@ export default class Building {
     constructor(sqft) {
       this._sqft = sqft;
   
-      // Déplace le bloc IF ici, juste après l'assignation
       if (this.constructor !== Building && !this.evacuationWarningMessage) {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
