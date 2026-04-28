3900bfbfa4f73699b0591db2904ea6a857f3ff47
add1ktion <antoine.bats@gmail.com>
2026-04-28 10:02:53 +0200

    P1

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -6,6 +6,26 @@ export default class Pricing {
       this._currency = currency;
     }
   
+    get amount() {
+      return this._amount;
+    }
+  
+    set amount(val) {
+      if (typeof val !== 'number')
+        throw new TypeError('Amount must be a number');
+      this._amount = val;
+    }
+  
+    get currency() {
+      return this._currency;
+    }
+  
+    set currency(val) {
+      if (!(val instanceof Currency))
+        throw new TypeError('Currency must be of type Currency');
+      this._currency = val;
+    }
+
     displayFullPrice() {
       return `${this._amount} ${this._currency.displayFullCurrency()}`;
     }
--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,9 +2,17 @@ export default class Building {
     constructor(sqft) {
       this._sqft = sqft;
   
-      if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      if (this.constructor !== Building && !this.constructor.prototype.hasOwnProperty('evacuationWarningMessage')) {
         throw new Error('Class extending Building must override evacuationWarningMessage');
       }
+    }
+
+    evacuationWarningMessage() {
+      return 'Evacuate immediately!';
+    }
+  
+    get evacuationWarning() {
+      return this.evacuationWarningMessage();
     }
   
     get sqft() {
