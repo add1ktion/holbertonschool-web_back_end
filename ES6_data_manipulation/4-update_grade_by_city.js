1609179007113d922bfe0e1e540fa98c8bf7d896
add1ktion <antoine.bats@gmail.com>
2026-05-07 09:27:04 +0200

    P0

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students) || !Array.isArray(newGrades)) {
+    return [];
+  }
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeInfo = newGrades.find((grade) => grade.studentId === student.id);
+
+      return {
+        ...student,
+        grade: gradeInfo ? gradeInfo.grade : 'N/A',
+      };
+    });
+}
---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') {
+    return '';
+  }
+  const result = [];
+  for (const element of set) {
+    if (typeof element === 'string' && element.startsWith(startString)) {
+      result.push(element.slice(startString.length));
+    }
+  }
+  return result.join('-');
+}
